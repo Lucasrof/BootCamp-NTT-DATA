fdc4f728a1144255ab191bba1ba76bdf194488d2
Lucas Rosa <lucasrof7@gmail.com>
2024-09-08 10:25:12 -0300

    feature:funcao-conta-corrente-limite-saldo-e-saque

--- appbank.py
+++ appbank.py
@@ -97,6 +97,41 @@ class Conta:
             return False
         return True
 
+class conta_corrente(Conta):
+    def __init__(self, numero, cliente, limite=500, limite_saques=3):
+        super().__init__(numero, cliente)
+        self._limite = limite
+        self._limite_saques = limite_saques
+    
+    @classmethod
+    def nova_conta(cls, cliente, numero, limite, limite_saques):
+        return cls(numero, cliente, limite, limite_saques)
+    def sacar(self, valor):
+        numero_saques = len (
+            [
+                transacao
+                for transacao in self.historico.transacoes
+                if transacao["tipo"] == Saque.__name__
+            ]
+        )
+        excedeu_limite = valor > self._limite
+        excedeu_saques = numero_saques > self._limite_saques
+
+        if excedeu_limite:
+            print("\n@@@ Operação falhou! O valor do saque excede o limite de sua conta. @@@")
+        elif excedeu_saques:
+            print("\n@@@ Operação falhou! Número de saques diários excedidos, contate seu banco!. @@@")
+        else:
+            return super().sacar(valor)
+        return False
+    
+    def __str__(self):
+        return f"""
+        Agência:\t{self.agencia}
+        C/C:\t\t{self.numero}
+        Titular:\t{self.cliente.nome}
+        """
+
 
 
 
